da7cd4ecb02e28524bdadb5f8a47504a79015151
Edi Permadi <edipermadi@gmail.com>
2023-05-10 13:08:13 +0800

    m5stack-atom-u refactor rgb animation

--- boards/m5stack-atom-u/builtin-led-rgb-sk6812/main.py
+++ boards/m5stack-atom-u/builtin-led-rgb-sk6812/main.py
@@ -6,6 +6,7 @@ from neopixel import NeoPixel
 pin = Pin(27, Pin.OUT)
 np = NeoPixel(pin, 1)  # 1 LED only
 
+# color choices
 choices = [
     [0, 0, 51],
     [0, 51, 0],
@@ -16,39 +17,37 @@ choices = [
     [51, 51, 51],
 ]
 
+
+def increase(value, new_value):
+    return value + 1 if value < new_value else value
+
+
+def decrease(value):
+    return value - 1 if value > 0 else value
+
+
+def update(red, green, blue):
+    np[0] = (red * 5, green * 5, blue * 5, 0)
+    np.write()
+
+
 # pick color, brighten up then fade away
 while True:
     red, green, blue = 0, 0, 0
     (new_red, new_green, new_blue) = random.choice(choices)
 
+    time.sleep(0.2)
     for i in range(51):
-        if red < new_red:
-            red += 1
-
-        if green < new_green:
-            green += 1
-
-        if blue > new_blue:
-            blue -= 1
-
-        np[0] = (red * 5, green * 5, blue * 5, 0)
-        np.write()
+        red = increase(red, new_red)
+        green = increase(green, new_green)
+        blue = increase(blue, new_blue)
+        update(red, green, blue)
         time.sleep(0.05)
 
     time.sleep(0.2)
-
     for i in range(51):
-        if red > 0:
-            red -= 1
-
-        if green > 0:
-            green -= 1
-
-        if blue > 0:
-            blue -= 1
-
-        np[0] = (red * 5, green * 5, blue * 5, 0)
-        np.write()
+        red = decrease(red)
+        green = decrease(green)
+        blue = decrease(blue)
+        update(red, green, blue)
         time.sleep(0.05)
-
-    time.sleep(0.2)
